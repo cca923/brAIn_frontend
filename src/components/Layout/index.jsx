a3787326ed601861c50eed21e948d61344d406da
cca923 <anna85923@gmail.com>
2025-03-11 13:21:28 +0800

    feat: update path

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
   return (
     <LayoutContainer>
       <div className="logo-container">
-        <img className="logo-img" src="public/images/logo.png" alt="logo" />
+        <img className="logo-img" src="/images/logo.png" alt="logo" />
       </div>
       <div className="page">{children}</div>
     </LayoutContainer>

d6e4c05b201af82036dd40c7f647f9833041ee2c
cca923 <anna85923@gmail.com>
2025-04-08 14:58:19 +0800

    feat: disabled btn if no files

--- src/containers/Buttons/ChatButton/index.jsx
+++ src/containers/Buttons/ChatButton/index.jsx
@@ -1,23 +1,27 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { IoChatboxEllipsesOutline } from "react-icons/io5";
 
 import ActionButton from "../../../components/ActionButton";
 import { Icon } from "../../../styles/common";
+import { fileSelector } from "../../../store/selectors";
 import { handleStartChat } from "../../../store/chat/service";
 
 const ChatButton = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { files } = useSelector(fileSelector);
+  const disabled = files?.length === 0;
 
   const handleClick = () => {
+    if (disabled) return;
     // No need to await, chat page has loading ui
     dispatch(handleStartChat());
     navigate("/chat");
   };
 
   return (
-    <ActionButton onClick={handleClick}>
+    <ActionButton onClick={handleClick} disabled={disabled}>
       <Icon fontSize="20px" mr="5px">
         <IoChatboxEllipsesOutline />
       </Icon>
